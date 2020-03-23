3a0daa925b2a47c8b2beb0ca8a398064a00cb79f
jeffreymzd <mzhde@hotmail.com>
2020-03-22 21:53:50 -0400

    Initial project

---
+++ src/test/java/com/github/jeffrey/spring/boot/sfg/jms/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jeffrey.spring.boot.sfg.jms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
